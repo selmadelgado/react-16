015b2e9f0bcd5307ff385a44424661131765d0bb
Selma Delgado <selmadelgado@gmail.com>
2018-09-27 20:34:28 -0500

    Added Pseudo Selectors to Button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -42,16 +42,9 @@ class App extends Component {
   }
   
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
+    let btnClass = '';
+
 
     if (this.state.showPersons) {
       persons = (
@@ -67,25 +60,25 @@ class App extends Component {
         </div> 
       );
 
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if(this.state.persons.length <= 2) {
-      classes.push('red'); // classes = ['red']
+      assignedClasses.push( classes.red ); // classes = ['red']
     }
     if(this.state.persons.length <= 1) {
-      classes.push('bold'); // classes = ['red', 'bold']
+      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
     }
     
 
     return (
-        <div className="App">
+        <div className={classes.App}>
         <h1>Hello, Leo!</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
 
-        <button 
-        style={style}
+        <button
+        className={btnClass}
         onClick={this.togglePersonsHandler}>Toggle Persons</button>
 
         {persons}
