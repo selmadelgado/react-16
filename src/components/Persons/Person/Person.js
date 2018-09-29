c3cc3b8e75e2142d3555249a0c1f4a42470b9454
Selma Delgado <selmadelgado@gmail.com>
2018-09-29 14:36:47 -0500

    Use Ref to create focus on first element

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -19,6 +19,9 @@ class Person extends Component {
     
       componentDidMount(){
         console.log('[Person.js] Inside componentDidMount()');
+        if (this.props.position === 0) {
+            this.inputElement.focus();
+        }
       }
 
 
@@ -28,9 +31,11 @@ class Person extends Component {
             <Aux>
                <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
                <p>{this.props.children}</p>
-               <input type="text" 
-               onChange={this.props.changed}
-               value={this.props.name}/>
+               <input 
+                    ref={(inp) => { this.inputElement = inp}}
+                    type="text" 
+                    onChange={this.props.changed}
+                    value={this.props.name}/>
             </Aux>
         )
     }
