74802f6ac80ccfc784420b2895be0e2eb954c74d
Selma Delgado <selmadelgado@gmail.com>
2018-09-28 21:53:18 -0500

    Added Cockpit component and Cockpit CSS

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -43,41 +44,21 @@ class App extends Component {
   
   render() {
     let persons = null;
-    let btnClass = '';
-
+    
     if (this.state.showPersons) {
-      persons = (
-        <div>
-        <Persons 
+      persons = <Persons 
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
-          changed={this.nameChangeHandler}/>
-        </div> 
-      );
-
-      btnClass = classes.Red;
+          changed={this.nameChangeHandler}/>;
     }
-
-    const assignedClasses = [];
-    if(this.state.persons.length <= 2) {
-      assignedClasses.push( classes.red ); // classes = ['red']
-    }
-    if(this.state.persons.length <= 1) {
-      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
-    }
-    
 
     return (
         <div className={classes.App}>
-        <h1>Hello, Leo!</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-
-        <button
-        className={btnClass}
-        onClick={this.togglePersonsHandler}>Toggle Persons</button>
-
+        <Cockpit 
+          showPersons={this.state.showPersons} 
+            persons={this.state.persons}
+            clicked={this.togglePersonsHandler}/>
         {persons}
-
         </div>
     );
   }
