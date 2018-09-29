de790acce061f8251c039f6ac7ba0a6844aa0bd8
Selma Delgado <selmadelgado@gmail.com>
2018-09-29 12:22:20 -0500

    CreateHigher Order Component for Person Component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends PureComponent {
   constructor(props){
@@ -80,14 +81,14 @@ class App extends PureComponent {
     }
 
     return (
-        <div className={classes.App}>
+        <WithClass classes={classes.App}>
         <button onClick={() => {this.setState({showPersons: true})}} >Show Persons</button>
         <Cockpit 
           showPersons={this.state.showPersons} 
             persons={this.state.persons}
             clicked={this.togglePersonsHandler}/>
         {persons}
-        </div>
+        </WithClass>
     );
   }
 }
