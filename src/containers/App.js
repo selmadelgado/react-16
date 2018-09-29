a4c0037b0fec9ed0a0e2880a1e897db813c6008f
Selma Delgado <selmadelgado@gmail.com>
2018-09-29 14:14:09 -0500

    Use prevState and setState to set state correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,7 +17,8 @@ class App extends PureComponent {
         { id: '002', name: 'Puppy', age: 4}
       ],
       otherState: 'some other value',
-      showPersons: false
+      showPersons: false,
+      toggleClicked: 0
     };
   }
 
@@ -68,7 +69,12 @@ class App extends PureComponent {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+    this.setState( (prevState, props) => {
+      return {
+      showPersons: !doesShow, 
+      toggleClicked: prevState.toggleClicked + 1 
+      }
+    });
   }
   
   render() {
