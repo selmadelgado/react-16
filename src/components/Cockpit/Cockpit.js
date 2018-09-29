16e65d283c59530ec5c020d6c25cf72d4aaab677
Selma Delgado <selmadelgado@gmail.com>
2018-09-29 11:39:28 -0500

    Add Auxjs to Return Adjacent Elements

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './Cockpit.css';
+import Aux from '../../hoc/Aux';
 
 const cockpit = (props) => {
     const assignedClasses = [];
-    let btnClass = '';
+    let btnClass = classes.Button;
     if (props.showPersons){
-        btnClass = classes.Red;
+        btnClass = [classes.Button, classes.Red].join(' ');
     }
     
     if(props.persons.length <= 2) {
@@ -16,15 +17,14 @@ const cockpit = (props) => {
     }
 
     return(
-        <div className={classes.Cockpit}>
+        <Aux>
         <h1>Hello, Leo!</h1>
         <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button
         className={btnClass}
         onClick={props.clicked}>Toggle Persons</button>
-        </div>
+       </Aux>
     );
 };
-
 
 export default cockpit;
