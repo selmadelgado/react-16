65b2d971658cf6d1e6ab4d0122b5c3f4c244ba76
Selma Delgado <selmadelgado@gmail.com>
2018-09-29 19:55:46 -0500

    Update with React createRef method

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,10 +5,10 @@ import withClass from '../../../hoc/withClass';
 import Aux from '../../../hoc/Aux';
 
 class Person extends Component {
-
     constructor(props){
         super(props);
         console.log('[Person.js] Inside constructor', props);
+        this.inputElement = React.createRef();
       }
     
       componentWillMount(){
@@ -18,7 +18,7 @@ class Person extends Component {
       componentDidMount(){
         console.log('[Person.js] Inside componentDidMount()');
         if (this.props.position === 0) {
-            this.inputElement.focus();
+          this.inputElement.current.focus();
         }
       }
 
@@ -30,7 +30,7 @@ class Person extends Component {
                <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
                <p>{this.props.children}</p>
                <input 
-                    ref={(inp) => { this.inputElement = inp}}
+                    ref={this.inputElement}
                     type="text" 
                     onChange={this.props.changed}
                     value={this.props.name}/>
